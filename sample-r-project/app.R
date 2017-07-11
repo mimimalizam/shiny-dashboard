4c3bf2aa8b93e547f340dabed1d1afe6cf0ebdac
Milana <milana.stojadinov@gmail.com>
2017-07-11 10:20:23 +0200

    Add interactive chart

--- sample-r-project/app.R
+++ sample-r-project/app.R
@@ -1,12 +1,29 @@
-library(shiny)
 library(shinydashboard)
 
 ui <- dashboardPage(
-  dashboardHeader(),
+  dashboardHeader(title = "Basic dashboard"),
   dashboardSidebar(),
-  dashboardBody()
+  dashboardBody(
+    # Boxes need to be put in a row (or column)
+    fluidRow(
+      box(plotOutput("plot1", height = 250)),
+      
+      box(
+        title = "Controls",
+        sliderInput("slider", "Number of observations:", 1, 100, 50)
+      )
+    )
+  )
 )
 
-server <- function(input, output) { }
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
 
 shinyApp(ui, server)
